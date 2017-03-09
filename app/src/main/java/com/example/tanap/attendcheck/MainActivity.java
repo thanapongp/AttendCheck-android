e2d02a1df0332166ee0c9718400c07891dd459e6
Thanapong Prathumchat <thanapong.pr.56@ubu.ac.th>
2017-03-09 22:12:12 +0700

    Add MainActivity initial layout

--- app/src/main/java/com/example/tanap/attendcheck/MainActivity.java
+++ app/src/main/java/com/example/tanap/attendcheck/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.tanap.attendcheck;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    
+    public void performAttendanceCheck(View view) {
+        Toast.makeText(this, "PRESSED!", Toast.LENGTH_SHORT).show();
     }
 }
